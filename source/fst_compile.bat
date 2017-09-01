0c8664625cb7c053da1d15f77d6c431868fad06d
700hours <31488826+700hours@users.noreply.github.com>
2017-08-31 21:13:14 -0700

    Delete fst_compile.bat

--- source/fst_compile.bat
+++
@@ -1,3 +0,0 @@
-cd %WINDIR%/Microsoft.net/Framework/v4.0.30319
-csc /r:System.dll /r:System.Net.dll /t:exe %userprofile%\Desktop\source\fst_AssemblyInfo.cs %userprofile%\Desktop\source\FileStream.cs
-pause
